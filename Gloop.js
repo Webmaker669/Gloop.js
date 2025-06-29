1b500255976ed3f3e5dad8b3be88a5419a2a9652
Webmaker669 <ethynh2010@gmail.com>
2025-06-29 15:26:35 -0400

    Create Gloop.js

---
+++ Gloop.js
@@ -0,0 +1,37 @@
+elements.gloop = {
+    color: "#6a4f4b",
+    behavior: behaviors.STICKY,
+    category: "liquids",
+    state: "liquid",
+    viscosity: 10000,
+    density: 1200,
+    tempHigh: 200,
+    stateHigh: "gas",
+    reactions: {
+        "sparkdust": { elem1: null, elem2: "boomputty" },
+    }
+};
+
+elements.sparkdust = {
+    color: "#ffcc00",
+    behavior: behaviors.POWDER,
+    category: "powders",
+    state: "solid",
+    density: 500,
+    reactions: {
+        "gloop": { elem1: null, elem2: "boomputty" },
+    }
+};
+
+elements.boomputty = {
+    color: "#c00000",
+    behavior: behaviors.STICKY,
+    category: "explosives",
+    state: "solid",
+    density: 1500,
+    tempHigh: 100,
+    stateHigh: "explosion",
+    burn: 90,
+    burnTime: 50,
+    fireColor: "#ff2200",
+};
