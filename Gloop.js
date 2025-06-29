2eb1636049aa6c534479d9fde7c15ff42ee81644
Webmaker669 <ethynh2010@gmail.com>
2025-06-29 15:29:48 -0400

    Update Gloop.js

--- Gloop.js
+++ Gloop.js
@@ -1,4 +1,5 @@
-elements.gloop = {
+runAfterLoad(function() {
+  elements.gloop = {
     color: "#6a4f4b",
     behavior: behaviors.STICKY,
     category: "liquids",
@@ -7,23 +8,19 @@ elements.gloop = {
     density: 1200,
     tempHigh: 200,
     stateHigh: "gas",
-    reactions: {
-        "sparkdust": { elem1: null, elem2: "boomputty" },
-    }
-};
+    reactions: { "sparkdust": { elem1: null, elem2: "boomputty" } }
+  };
 
-elements.sparkdust = {
+  elements.sparkdust = {
     color: "#ffcc00",
     behavior: behaviors.POWDER,
     category: "powders",
     state: "solid",
     density: 500,
-    reactions: {
-        "gloop": { elem1: null, elem2: "boomputty" },
-    }
-};
+    reactions: { "gloop": { elem1: null, elem2: "boomputty" } }
+  };
 
-elements.boomputty = {
+  elements.boomputty = {
     color: "#c00000",
     behavior: behaviors.STICKY,
     category: "explosives",
@@ -34,4 +31,5 @@ elements.boomputty = {
     burn: 90,
     burnTime: 50,
     fireColor: "#ff2200",
-};
+  };
+});
